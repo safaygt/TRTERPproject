6b2858d3dbb44a107fa87cb78f31b238450e2e64
celenibrahim <celen.ibrahim@outlook.com>
2024-12-20 21:05:14 +0300

    added raw login page and main page

--- TRTERPproject/Form1.Designer.cs
+++ TRTERPproject/Form1.Designer.cs
@@ -1,39 +1,64 @@
 ﻿namespace TRTERPproject
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			button_giris = new Button();
+			SuspendLayout();
+			// 
+			// button_giris
+			// 
+			button_giris.Location = new Point(396, 565);
+			button_giris.Name = "button_giris";
+			button_giris.Size = new Size(189, 73);
+			button_giris.TabIndex = 0;
+			button_giris.Text = "Giriş";
+			button_giris.UseVisualStyleBackColor = true;
+			button_giris.Click += button_giris_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(971, 681);
+			Controls.Add(button_giris);
+			DoubleBuffered = true;
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private Button button_giris;
+	}
 }
